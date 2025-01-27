238931263adcd2a35fdca008b88ca4c53e9b893c
JoshK7 <joshk7683@gmail.com>
2025-01-26 22:23:48 -0600

    added logic to properly calculate tip

--- script.js
+++ script.js
@@ -1,98 +1,209 @@
 const tipForm = document.querySelector("form");
-const percentages = tipForm.querySelectorAll("input[name=percent]");
-const numberInputs = tipForm.querySelectorAll("input[type=number]");
-const reset = document.getElementById("reset");
 const billInput = document.getElementById("bill");
+const percentageLabels = tipForm.querySelectorAll(".percentage__label");
+const percentages = tipForm.querySelectorAll(".percentage");
+const custom = document.getElementById("custom");
 const peopleInput = document.getElementById("people");
+const reset = document.getElementById("reset");
+const numberInputs = tipForm.querySelectorAll("input[type=number]");
+const error = document.getElementById("error");
+const tip = document.getElementById("tip");
+const total = document.getElementById("total");
 
-var clicked;
+var unClicked = true;
 
-const clearError = () => {
-    console.log("no error!");
-}
+const labelPercent = {};
+percentages.forEach((input) => {
+    labelPercent[input.id] = input.value;
+});
 
-const renderError = () => {
-    console.log("error!")
-}
+const getPercentage = () => {
+    if (custom.value) {
+        return parseFloat((custom.value / 100));
+    }
 
-const digits = {
-    '0': 0,
-    '1': 1,
-    '2': 2,
-    '3': 3,
-    '4': 4,
-    '5': 5,
-    '6': 6,
-    '7': 7,
-    '8': 8,
-    '9': 9
-}
+    for (const button of percentageLabels) {
+        if (button.classList.contains("clicked")) {
+            const key = button.getAttribute("for");
+            return parseFloat(labelPercent[key]);
+        }
+    }
+};
 
 const inputLimit = {
-    "bill": 10000000000000,
-    "custom": 100,
-    "people": 100000,
-}
+    bill: 10000000,
+    custom: 1000,
+    people: 100000,
+};
 
-const handleKeyPress = (e) => {
-    if (e.key === "." || e.key === "-") {
+const handleInputLimit = (e) => {
+    if (e.key === "-") {
         e.preventDefault();
     }
 
-    if (e.key in digits && parseInt(e.target.value + e.key) > inputLimit[e.target.id]) {
-        e.preventDefault()
+    if (e.target.id === "people" && e.key === ".") {
+        e.preventDefault();
     }
+
+    if (e.key === "Backspace") {
+        return;
+    }
+
+    const value = e.target.value;
+    const components = value.split(".");
+
+    if (components.length > 1 && components[1].length === 2) {
+        e.preventDefault();
+    }
+
+    if (parseFloat(value + e.key) > inputLimit[e.target.id]) {
+        e.preventDefault();
+    }
+};
+
+const formData = () => {
+    return {
+        bill: billInput.value ? parseFloat(billInput.value): 0,
+        percent: getPercentage(),
+        people: peopleInput.value ? parseFloat(peopleInput.value) : 0,
+    };
+};
+
+const resetPercentButtons = () => {
+    unClicked = true;
+    percentageLabels.forEach((button) => {
+        button.classList.remove("clicked");
+    });
+};
+
+const resetCustom = () => {
+    custom.value = "";
+    custom.classList.remove("outlined");
+};
+
+const resetTip = () => {
+    tip.innerText = "$0.00";
 }
 
-const handlePercentageClick = (e) => {
-    if (clicked) {
-        if (clicked.id === "custom") {
-            clicked.value = "";
-        }
-        clicked.classList.toggle("clicked");
-    }
+const resetTotal = () => {
+    total.innerText = "$0.00";
+}
 
+const disableButton = () => {
+    if (
+        billInput.value === "" &&
+        unClicked &&
+        custom.value === "" &&
+        peopleInput.value === ""
+    ) {
+        reset.disabled = true;
+    } else {
+        reset.disabled = false;
+    }
+};
+
+const clearError = () => {
+    error.classList.add("invisible");
+    peopleInput.classList.remove("error__input");
+    console.log("no error!");
+};
+
+const renderError = () => {
+    error.classList.remove("invisible");
+    peopleInput.classList.add("error__input");
+    console.log("error!");
+};
+
+const validate = (bill, people) => {
+    return !bill || people;
+};
+
+const calculateTip = (bill, percentage = 0, people) => {
+    const tip = (bill * percentage) / people;
+    return tip.toFixed(2);
+};
+
+const calculateTotal = (bill, people) => {
+    const total = bill / people;
+    return total.toFixed(2);
+};
+
+const handleBillInput = (e) => {
+    disableButton();
+}
+
+const handleRadioClick = (e) => {
+    resetCustom();
+    resetPercentButtons();
+    unClicked = false;
     e.target.classList.toggle("clicked");
-    clicked = e.target;
-}
+    disableButton();
+};
 
-const handlePercentageKeypress = (e) => {
+const handleRadioKeypress = (e) => {
     if (e.key === "Enter") {
         e.target.click();
     }
-}
+};
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = {};
-    const fields = tipForm.querySelectorAll("input");
-    for (const field of fields) {
-        formData[field.name] = field.value;
-    }
+const handleCustomClick = (e) => {
+    resetPercentButtons();
+    custom.classList.toggle("outlined");
+};
 
-    if (formData.bill && clicked && !formData.people) {
-        renderError();
-    } else {
-        clearError();
-    }
+const handleCustomInput = (e) => {
+    disableButton();
+};
 
-    console.log(formData);
-}
+const handlePeopleInput = (e) => {
+    disableButton();
+};
 
 const handleResetClick = (e) => {
-    // doesn't work :(
-    tipForm.reset();
+    billInput.value = "";
+    resetPercentButtons();
+    resetCustom();
+    peopleInput.value = "";
+    resetTip();
+    resetTotal();
+    clearError();
+    disableButton();
+};
+
+const handleFormInput = (e) => {
+    const { bill, percent, people } = formData();
+    const valid = validate(bill, people);
+    if (bill === 0) {
+        return;
+    }
+
+    if (!valid) {
+        renderError();
+    } else {
+        const tipAmount = calculateTip(bill, percent, people);
+        const totalAmount = calculateTotal(bill, people);
+        tip.innerText = `$${tipAmount}`;
+        total.innerText = `$${totalAmount}`;
+        clearError(); 
+    }
 }
 
-percentages.forEach((button) => {
-    button.addEventListener("click", handlePercentageClick);
-    button.addEventListener("keypress", handlePercentageKeypress);
+billInput.addEventListener("input", handleBillInput);
+
+percentageLabels.forEach((button) => {
+    button.addEventListener("click", handleRadioClick);
+    button.addEventListener("keypress", handleRadioKeypress);
 });
+
+custom.addEventListener("click", handleCustomClick);
+custom.addEventListener("input", handleCustomInput);
+
+peopleInput.addEventListener("input", handlePeopleInput);
+
+reset.addEventListener("click", handleResetClick);
 
 numberInputs.forEach((input) => {
-    input.addEventListener("keydown", handleKeyPress);
+    input.addEventListener("keydown", handleInputLimit);
 });
 
-// tipForm.addEventListener("change", handleChange);
-tipForm.addEventListener("submit", handleSubmit)
-reset.addEventListener("click", handleResetClick);
+tipForm.addEventListener("input", handleFormInput);
